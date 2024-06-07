7b5906c86a3129ba049eb0f4fd683103ea2d78a8
Emaan Riaz <emaanriaz@Emaans-MacBook-Air.local>
2024-06-07 00:58:38 -0700

    change question

--- src/components/PromptCards.tsx
+++ src/components/PromptCards.tsx
@@ -2,14 +2,14 @@ const PromptCards = ({ handleSubmit }: any) => {
   return (
     <div className="grid md:grid-cols-2 grid-cols-1 gap-4">
       <div
-        onClick={() => handleSubmit("What's today's date?")}
+        onClick={() => handleSubmit("I need help with a task")}
         className="flex flex-col justify-between p-4 h-[10rem] rounded-xl border cursor-pointer duration-150 ease-in transition-border hover:scale-[1.015] shadow-sm"
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24">
           <path stroke="#1C274C" strokeWidth="1.5" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12c0 1.6.376 3.112 1.043 4.453.178.356.237.763.134 1.148l-.595 2.226a1.3 1.3 0 0 0 1.591 1.591l2.226-.595a1.634 1.634 0 0 1 1.149.133A9.958 9.958 0 0 0 12 22Z"/>
           <path stroke="#1C274C" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 12h.009m3.982 0H12m3.991 0H16" opacity=".5"/>
         </svg>
-        What's today's date?
+        I need help with a task
       </div>
 
       <div
